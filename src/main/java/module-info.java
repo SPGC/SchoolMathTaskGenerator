c007832db8cd6b3a1418294320f84761ca659ced
SPGC <lelikk2002@gmail.com>
2022-09-14 22:14:48 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.schoolmathtaskgeneratot {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.example.schoolmathtaskgeneratot to javafx.fxml;
+    exports com.example.schoolmathtaskgeneratot;
+}
